bf4fc883177616dd28f63ec87d835a48cf3ac27c
Kapish Singh <kapishsingh2209@gmail.com>
2022-03-07 21:11:52 +0530

    Credflow Task (release): v0.1.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Product from "./components/Product";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Product />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
