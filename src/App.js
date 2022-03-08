a2c4ef54922b823c7f71ab4c5aba2bc6984bbee7
Kapish Singh <kapishsingh2209@gmail.com>
2022-03-08 08:06:24 +0530

    Credflow Task (release): v0.1.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Product from "./components/Product";
 import AddDelete from "./components/AddDelete.jsx";
 import Checkout from "./components/Checkout.jsx";
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Product />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <button
+                onClick={() => {
+                  navigate("/product");
+                }}
+              >
+                Payemnts
+              </button>
+              <Product />
+            </>
+          }
+        />
         <Route path="/product" element={<AddDelete />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
